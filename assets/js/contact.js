a59956ad670e80d31749256f26f538e608654dec
David Cassiet <davidcassiet@gmail.com>
2022-09-20 12:14:14 -0300

    Responsive modifications

--- assets/js/contact.js
+++
@@ -1,8 +0,0 @@
-const contactButton = document.querySelector(".contact__button");
-
-contactButton.addEventListener("click", () => {
-  contactButton.classList.add("contact__button-active");
-  setTimeout(() => {
-    contactButton.classList.remove("contact__button-active");
-  }, 500);
-});
